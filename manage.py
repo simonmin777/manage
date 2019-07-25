f5cc20aca2eee08b38e90a8b048d2ceb237bcaf8
Min Cheng <simonmin777@gmail.com>
2019-07-25 12:41:12 +0800

    remove delta of water cycle, bug fix

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 """
-top level to use house module
+top level to use excel module
 
-manage filename -e ebill
+python3 manage.py -i
 rbiz07.645sj@gmail.com
 
 vervion 1.0 released on 2018.01.01
 version 1.2 released on 2019.02.15
+version 1.3 released on 2019.07.24
 """
 
 import logging
@@ -14,6 +15,7 @@ import time
 import openpyxl
 import smtplib
 import excel
+import datetime
 
 # gloable error code here
 ERROR_WRONG_CMD = 101
@@ -45,9 +47,21 @@ def send_gmail_all(myaddress, mypassword, tenant_list, flag=True):
             send_gmail(myaddress, mypassword, tenant.email,
                        'Utility bill due %s' % tenant.service_cycle.get_billday_string(), tenant.get_email_txt(), flag)
             print('E-maind sent successfully ==> %s [%s %s]' % (tenant.email, tenant.room, tenant.name))
-        else:
-            print('\nWarning: NOT SEND e-mail to %s [%s %s]\n' % (tenant.email, tenant.room, tenant.name))
 
+def send_gmail_m_all(myaddress, mypassword, tenant_list, target_date, flag=True):
+    """ send maintaince email to all tenant in a tenant list """
+    for tenant in tenant_list:
+        if tenant.sendemail:
+            # TODO: format email + 2day for room not start with '645' & '632', logic here is not correct
+            monday_list = ['645', '632']
+            for _ in monday_list:
+                if _ == tenant.room[:3]:
+                    target_date += datetime.timedelta(days=2)
+                    break
+
+            send_gmail(myaddress, mypassword, tenant.email,
+                       'Utility bill due %s' % tenant.service_cycle.get_billday_string(), tenant.get_email_txt(), flag)
+            print('E-maind sent successfully ==> %s [%s %s]' % (tenant.email, tenant.room, tenant.name))
 
 def ask_confirm(action):
     strin = ' '
@@ -82,7 +96,7 @@ if __name__ == '__main__':
     def main():
         print(' ')
         filename = 'nosuchfile'
-        if len(sys.argv) < 2 or sys.argv[1].lower() != '-i':
+        if len(sys.argv) < 2 or (sys.argv[1].lower() != '-i' and sys.argv[1].lower() != '-m'):
             excel.Excel.error_exit('Usage: python manage.py -i [file.xlsx] [sheetname]', ERROR_WRONG_CMD)
         elif len(sys.argv) == 2:
             filename = 'OasisTenates.xlsx'
@@ -95,21 +109,33 @@ if __name__ == '__main__':
         if not test_open(filename, sheetname):
             excel.Excel.error_exit('%s [%s] has wrong format or not accessible' % (filename, sheetname), ERROR_WRONG_XLSX)
 
-        xlsx = excel.Excel(filename, sheetname)
-        xlsx.process()
-        print('\nCheck all tenants')
-        xlsx.tenant_check()
-        xlsx.write_all_tenant_to_file()
-        if ask_confirm('send e-mail'):
-            with open('config.txt') as fin:
-                email = fin.readline().strip()
-                password = fin.readline().strip()
-            send_gmail_all(email, password, xlsx.tenant, True)
-        if ask_confirm('backup xlsx'):
-            xlsx.backup(filename[:-5]+' backup.xlsx')
-        if ask_confirm('clean input xlsx'):
-            xlsx.cleanup()
-        xlsx.close()
+        if sys.argv[1].lower() == '-i':
+            xlsx = excel.Excel(filename, sheetname)
+            xlsx.process()
+            print('\nCheck all tenants')
+            xlsx.tenant_check()
+            xlsx.write_all_tenant_to_file()
+            if ask_confirm('send e-mail'):
+                with open('config.txt') as fin:
+                    email = fin.readline().strip()
+                    password = fin.readline().strip()
+                send_gmail_all(email, password, xlsx.tenant, True)
+            if ask_confirm('backup xlsx'):
+                xlsx.backup(filename[:-5]+' backup.xlsx')
+            if ask_confirm('clean input xlsx'):
+                xlsx.cleanup()
+            xlsx.close()
+        elif sys.argv[1].lower() == '-m':
+            xlsx = excel.Excel(filename, sheetname)
+            xlsx.process()
+            target_date = datetime.datetime(sys.argv[4], sys.argv[2], sys.argv[3])
+            print('Target date is %s' % str(target_date)[:10])
+            if ask_confirm('send e-mail'):
+                with open('config.txt') as fin:
+                    email = fin.readline().strip()
+                    password = fin.readline().strip()
+                send_gmail_m_all(email, password, xlsx.tenant, target_date, True)
+            xlsx.close()
 
     logging.basicConfig(level=logging.WARNING, format='%(levelname)s - %(message)s')
     main()
